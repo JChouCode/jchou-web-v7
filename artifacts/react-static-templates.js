6fecb04c1c5f91e802e7cd85b1f333141fef56ff
Jeffrey <jc2573@cornell.edu>
2019-12-08 02:58:39 -0500

    Fix footer color

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -1,35 +1,16 @@
 
 
-import React from 'react'
-import universal, { setHasBabelPlugin } from '/Users/jeffreychou/Desktop/jchou-web-v7/node_modules/react-universal-component/dist/index.js'
-
-setHasBabelPlugin()
-
-const universalOptions = {
-  loading: () => null,
-  error: props => {
-    console.error(props.error);
-    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
-  },
-  ignoreBabelRename: true
-}
-
-const t_0 = universal(import('../src/pages/404.js'), universalOptions)
-      t_0.template = '../src/pages/404.js'
-      
-const t_1 = universal(import('../src/pages/index.js'), universalOptions)
-      t_1.template = '../src/pages/index.js'
-      
-const t_2 = universal(import('../src/templates/post'), universalOptions)
-      t_2.template = '../src/templates/post'
-      
-
+  
 // Template Map
 export default {
-  '../src/pages/404.js': t_0,
-'../src/pages/index.js': t_1,
-'../src/templates/post': t_2
+  '../src/pages/404.js': require('../src/pages/404.js').default,
+'../src/pages/about.js': require('../src/pages/about.js').default,
+'../src/pages/blog.js': require('../src/pages/blog.js').default,
+'../src/pages/contact.js': require('../src/pages/contact.js').default,
+'../src/pages/index.js': require('../src/pages/index.js').default,
+'../src/pages/work.js': require('../src/pages/work.js').default,
+'../src/templates/post': require('../src/templates/post').default
 }
-// Not Found Template
-export const notFoundTemplate = "../src/pages/404.js"
+
+export const notFoundTemplate = '../src/pages/404.js'
 
